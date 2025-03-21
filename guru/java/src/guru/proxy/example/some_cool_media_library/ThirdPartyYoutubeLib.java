d6b99fd2fa4283a6ee6023bc6877d2774a35202b
Will Wu <talentwill@outlook.com>
2025-03-22 07:40:20 +0800

    Add 图书信息管理系统_flask_vue

--- guru/java/src/guru/proxy/example/some_cool_media_library/ThirdPartyYoutubeLib.java
+++
@@ -1,9 +0,0 @@
-package guru.proxy.example.some_cool_media_library;
-
-import java.util.HashMap;
-
-public interface ThirdPartyYoutubeLib {
-    HashMap<String, Video> popularVideos();
-
-    Video getVideo(String videoId);
-}
